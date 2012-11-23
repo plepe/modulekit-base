2b726ac986b9993341f0184195c31a739789029b
Stephan Plepelits <skunk@xover.mud.at>
2012-11-23 07:16:29 +0100

    PG Schema: Read schemas from modulekit definition

--- modules/pg_schema/mcp.php
+++ modules/pg_schema/mcp.php
@@ -11,6 +11,24 @@ function register_pg_schema($schema_id) {
   }
 }
 
+function pg_schema_update() {
+  global $modulekit;
+  global $pg_schemas;
+  global $pg_schemas_tr;
+
+  foreach($modulekit['order'] as $module_id) {
+    $module_data=modulekit_loaded($module_id);
+
+    if(isset($module_data['pg_schema'])) {
+      if(is_array($module_data['pg_schema']))
+	foreach($module_data['pg_schema'] as $schema_id)
+	  register_pg_schema($schema_id);
+      else
+	  register_pg_schema($module_data['pg_schema']);
+    }
+  }
+}
+
 function pg_schema_expand_schemas($qry, $conn) {
   global $pg_schemas_tr;
 
@@ -21,5 +39,6 @@ if(!isset($pg_schemas))
   $pg_schemas=array();
 if(!isset($pg_schemas['user']))
   register_pg_schema("user", $db['user']); // default user schema
+pg_schema_update();
 
 register_hook("pg_sql_query", "pg_schema_expand_schemas");
