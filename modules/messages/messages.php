712b070d08e9da0cac1db5e539942560e7a981e9
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2013-08-26 13:46:51 +0200

    Messages: new module

---
+++ modules/messages/messages.php
@@ -0,0 +1,40 @@
+<?
+define(MSG_ERROR, "error");
+define(MSG_NOTICE, "notice");
+global $messages_keep;
+$messages_keep=false;
+
+function messages_add($text, $level=MSG_NOTICE) {
+  if(!isset($_SESSION['messages']))
+    $_SESSION['messages']=array();
+  if(!isset($_SESSION['messages'][$level]))
+    $_SESSION['messages'][$level]=array();
+
+  $_SESSION['messages'][$level][]=$text;
+}
+
+function messages_print() {
+  if(!isset($_SESSION['messages']))
+    return "";
+
+  $ret ="<div class='messages'>\n";
+  foreach($_SESSION['messages'] as $level=>$msgs) {
+    $ret.="  <ul class='$level'>\n";
+    foreach($msgs as $msg) {
+      $ret.="  <li>$msg</li>\n";
+    }
+    $ret.="  </ul>\n";
+  }
+  $ret.="</div>\n";
+
+  global $messages_keep;
+  if(!$messages_keep)
+    unset($_SESSION['messages']);
+
+  return $ret;
+}
+
+function messages_keep() {
+  global $messages_keep;
+  $messages_keep=true;
+}
---
+++ modules/messages/modulekit.php
@@ -0,0 +1,6 @@
+<?
+$include=array(
+  'php'=>array(
+    'messages.php',
+  ),
+);
