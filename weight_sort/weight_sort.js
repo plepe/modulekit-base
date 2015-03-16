d7e07031501589671f325433c11a5c9ed0b60476
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2015-03-16 14:38:26 +0100

    weight_sort: (JS) also allow 2nd form of elements (as in php version)

--- weight_sort/weight_sort.js
+++ weight_sort/weight_sort.js
@@ -2,10 +2,15 @@
 // Parameters:
 // arr ... an array of form [ [ weight, var], ... ]
 //         [ [ -3, A ], [ -1, B ], [ 5, C ], [ -1, D ] ]
-//         
+//         OR
+//         an array of form [ { 'weight': value, other key/values } ]
+//         [ { 'weight': -3, k: A ], { 'weight': -1, v: B }, [ 5, C ] ]
+//
+//  as the last item shows, you can even mix the forms
+//
 // Returns:
-// An array sorted by the weight of the source, e.g.
-//         [ A, B, D, C ]
+// if the first form is used, only the 'var' will be returned; with the
+// second form the elements are untouched, only their position changed.
 //
 // Notes:
 // Entries in the source array with the same weight are returned in the
@@ -20,14 +25,25 @@ function weight_sort(arr) {
   // first put all elements into an assoc. array
   for(var i=0; i<arr.length; i++) {
     var cur=arr[i];
-    var wgt=cur[0];
+    var wgt;
+    var data;
+
+    if(cur.length) {
+      wgt = cur[0];
+      data = cur[1];
+    }
+    else {
+      wgt = cur.weight;
+      data = cur;
+    }
+
     if(!wgt)
       wgt=0;
 
     if(!ret1[wgt])
       ret1[wgt]=[];
 
-    ret1[wgt].push(cur[1]);
+    ret1[wgt].push(data);
   }
 
   // get the keys, convert to value, order them
