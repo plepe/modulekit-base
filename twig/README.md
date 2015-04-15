1ae9727ae3cc24eba94a76d0f0b30259f60366e2
Stephan Bösch-Plepelits <skunk@xover.htu.tuwien.ac.at>
2015-04-15 20:40:28 +0200

    Twig: new JS function `twig_render_into()`

--- twig/README.md
+++ twig/README.md
@@ -19,3 +19,9 @@ twig_render_custom(template, data)
 Pass the source of a template as first parameter, render with given data. Returns rendered output.
 
 Scope: PHP and JS
+
+twig_render_into(dom_node, template_id, data)
+---------------------------------------------
+Loads the file (e.g. 'footer.html') from `templates/`-directory and renders with the given data into the dom_node. Returns nothing. Rendering might not happen immediatly but after loading the template via a XMLHttpRequest.
+
+Scope: currently only in JS
--- twig/twig.js
+++ twig/twig.js
@@ -1,3 +1,21 @@
+var template_cache = {};
+
+function twig_render_into_final(dom_node, template_id, data, result) {
+  if(!(template_id in template_cache))
+    template_cache[template_id] = twig({ data: result });
+
+  dom_node.innerHTML = template_cache[template_id].render(data);
+}
+
+function twig_render_into(dom_node, template_id, data) {
+  if(template_id in template_cache) {
+    twig_render_into_final(dom_node, template_id, data);
+  }
+  else {
+    ajax('templates/' + template_id, null, twig_render_into_final.bind(this, dom_node, template_id, data));
+  }
+}
+
 function twig_render_custom(template, data) {
   var t = twig({ data: template });
   return t.render(data);
